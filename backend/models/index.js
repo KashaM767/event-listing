a20d8f5cd7a69b11d5f5f80b6f9361190a691ea7
Kasha Malloy <kashamalloy@gmail.com>
2024-08-02 15:58:12 +0100

    adding remote db

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://127.0.0.1:27017/event")
+mongoose.connect("mongodb+srv://kashamalloy:KZWjc1yXWHbGIt64@pixelcluster.eepusja.mongodb.net/?retryWrites=true&w=majority&appName=pixelCluster")
 
 module.exports.User = require("./user");
 module.exports.Event = require('./event');
